e2b6d3be35a26b44b6573bcfba7d49d854c49634
your-name <your_email_address@163.com>
2018-04-12 17:11:50 +0800

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
+from . models import Comment
 def home_page(request):
-
     if request.method == 'POST':
-        comment = request.POST.get('comment')
-        user = request.POST.get("username")
-
-        print(comment * 10)
-        return render(request, 'blog/home_page.html', {'comment': comment, 'username': user})
-
+        our_comment = request.POST.get('comment')
+        our_user = request.POST.get("username")
+        comment_obj = Comment.objects.create(
+            comment=our_comment,
+            user=our_user
+        )
+        return render(request, 'blog/home_page.html', {'comment': comment_obj,})
     return render(request, 'blog/home_page.html')
